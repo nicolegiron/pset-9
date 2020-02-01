06a4250bd0ea2982d958e04bb9800e352281d3db
nicolegiron1 <ngiron@ucvts.org>
2020-02-01 17:03:19 -0500

    finished exercise 1

--- src/Exercises.java
+++ src/Exercises.java
@@ -5,64 +5,63 @@ public class Exercises {
 	public boolean commonEnd(ArrayList<Integer> a, ArrayList<Integer> b) {
 		if (a == null || a.isEmpty() || b == null || b.isEmpty()) {
 			return false;
+		} else if (a.get(0) == b.get(0) || a.get(a.size()-1) == b.get(b.size()-1)) {
+			return true;
 		}
-		
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
+		return false;
 	}
-	
+
 	public ArrayList<String> endsMeet(ArrayList<String> values, int n) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
-	
+
 	public int difference(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public double biggest(ArrayList<Double> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public ArrayList<String> middle(ArrayList<String> values) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(ArrayList<Integer> numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(ArrayList<String> values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
